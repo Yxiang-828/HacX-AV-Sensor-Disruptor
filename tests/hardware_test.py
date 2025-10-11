a540b1de25610d81143818210bfcdb7a2e2faaba
yx <yx@email.com>
2025-10-11 23:13:23 +0800

    Update hardware_test.py to use EmitterDriver for emitter testing

--- tests/hardware_test.py
+++ tests/hardware_test.py
@@ -1,17 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 from utils import detect_signal
+from drivers import EmitterDriver
 
 GPIO.setmode(GPIO.BCM)
 PINS = [18, 19, 20, 21, 22]  # Laser, LED, RF, Switch, Photodiode
-GPIO.setup(PINS[:-2], GPIO.OUT)  # Emitters out
 GPIO.setup([PINS[-2], PINS[-1]], GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Switch + photodiode in
 
+# Initialize drivers for emitters
+laser = EmitterDriver(PINS[0])
+led = EmitterDriver(PINS[1])
+rf = EmitterDriver(PINS[2])
+
 print("Testing emitters...")
-for pin in PINS[:3]:
-    GPIO.output(pin, GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(pin, GPIO.LOW)
+laser.set_power(100); time.sleep(1); laser.set_power(0)
+led.set_power(100); time.sleep(1); led.set_power(0)
+rf.set_power(100); time.sleep(1); rf.set_power(0)
 
 print("Switch state:", GPIO.input(21))
 print("Photodiode state:", GPIO.input(22))
